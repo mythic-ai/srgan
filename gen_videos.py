b841568c4da659ef2f71b6da6819370301f526a7
Aram Ebtekar <aramebtech@gmail.com>
2018-05-03 16:35:20 -0700

    Complete script to process a video

--- gen_videos.py
+++ gen_videos.py
@@ -7,118 +7,92 @@ import scipy
 from model import SRGAN_g
 from timeit import default_timer as timer
 
-def shrink_video(video_path):
-    vidcap = cv2.VideoCapture(video_path)
-    if not vidcap.isOpened():
-        raise IOError(("Couldn't open video file or webcam. If you're "
-                       "trying to open a webcam, make sure your video_path is an integer!"))
+class SRGAN:
+    def __init__(self):
+        # Form the generator network.
+        self.t_image = tf.placeholder('float32', [None, None, None, 3], name='input_image')
+        self.net_g = SRGAN_g(self.t_image, is_train=False, reuse=False)
 
-    scale = 4
-    width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH) / scale)
-    height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT) / scale)
+        # Restore the generator weights.
+        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
+        tl.layers.initialize_global_variables(self.sess)
+        tl.files.load_and_assign_npz(sess=self.sess, name='checkpoint/g_srgan.npz', network=self.net_g)
 
-    # Define the codec and create VideoWriter objects
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    vid_lr = cv2.VideoWriter('samples/evaluate/lr.avi', fourcc, 20.0, (width, height))
+    def process_image(self, lr_frame):
+        # Do superresolution.
+        lr_frame = (lr_frame / 127.5) - 1
+        hr_frame = self.sess.run(self.net_g.outputs, {self.t_image: [lr_frame]})
+        hr_frame = (hr_frame + 1) * 127.5
+        hr_frame = np.uint8(hr_frame + 0.5)
+        return hr_frame[0]
 
-    while True:
-        flag, frame = vidcap.read()
+class Bicubic:
+    def __init__(self, scale):
+        self.scale = scale
+
+    def process_image(self, lr_frame):
+        h_scaled = int(lr_frame.shape[0] * self.scale)
+        w_scaled = int(lr_frame.shape[1] * self.scale)
+        hr_frame = scipy.misc.imresize(lr_frame, [h_scaled, w_scaled], interp='bicubic', mode=None)
+        return hr_frame
+
+class VideoFrames:
+    def __init__(self, video_path):
+        self.vidcap = cv2.VideoCapture(video_path)
+        if not self.vidcap.isOpened():
+            raise IOError(("Couldn't open video file or webcam. If you're "
+                           "trying to open a webcam, make sure your video_path is an integer!"))
+
+        self.width = int(self.vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        flag, frame = self.vidcap.read()
         if not flag:
-            print("Done!")
-            return
+            print("Finished reading video!")
+            self.vidcap.release()
+            raise StopIteration
+        return frame
 
-        lr_frame = scipy.misc.imresize(frame, [height, width], interp='bicubic', mode=None)
-        print(lr_frame.shape)
-        vid_lr.write(lr_frame)
+def process_video(video_path, lr_method, sr_methods, frame_skip=1):
+    # Open a video for reading
+    video_reader = VideoFrames(video_path)
+    print("Ground Truth resolution = {} x {}".format(video_reader.width, video_reader.height))
 
-    vidcap.release()
-    vid_lr.release()
+    for frame_count, lr_frame in enumerate(video_reader):
+        if frame_count % frame_skip == 0:
+            if lr_method is not None:
+                lr_frame = lr_method.process_image(lr_frame)
+            sr_frames = []
+            for sr_method in sr_methods:
+                sr_frame = sr_method.process_image(lr_frame)
+                sr_frames.append(sr_frame)
 
-def video_superresolution(video_path):
-    vidcap = cv2.VideoCapture(video_path)
-    if not vidcap.isOpened():
-        raise IOError(("Couldn't open video file or webcam. If you're "
-                       "trying to open a webcam, make sure your video_path is an integer!"))
-
-    width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    scale = 4
-
-    # Define the codec and create VideoWriter objects
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    vidbic = cv2.VideoWriter('samples/evaluate/bic.avi', fourcc, 20.0, (width * scale, height * scale))
-    vidgen = cv2.VideoWriter('samples/evaluate/gen.avi', fourcc, 20.0, (width * scale, height * scale))
-
-    # Form the generator network.
-    t_image = tf.placeholder('float32', [None, None, None, 3], name='input_image')
-    net_g = SRGAN_g(t_image, is_train=False, reuse=False)
-
-    # Restore the generator weights.
-    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
-    tl.layers.initialize_global_variables(sess)
-    checkpoint_dir = "checkpoint"
-    tl.files.load_and_assign_npz(sess=sess, name=checkpoint_dir + '/g_srgan.npz', network=net_g)
-
-    i = 0
-    while True:
-        i += 1
-        flag, lr_frame = vidcap.read()
-        if not flag:
-            print("Done!")
-            return
-
-        bic_frame = scipy.misc.imresize(lr_frame, [height * scale, width * scale], interp='bicubic', mode=None)
-        vidbic.write(bic_frame)
-        print("frame {}".format(i))
-
-        if i % 1 == 0:
-            # Do superresolution.
-            lr_frame = (lr_frame / 127.5) - 1
-            hr_frame = sess.run(net_g.outputs, {t_image: [lr_frame]})
-            hr_frame = (hr_frame + 1) * 127.5
-            hr_frame = np.uint8(hr_frame + 0.5)
-
-            print(hr_frame.shape)
-            print(hr_frame.min())
-            print(hr_frame.max())
-
-            cv2.imshow('frame', hr_frame[0])
+            sr_joined = np.concatenate(sr_frames, axis=1)
+            cv2.imshow('frame', sr_joined)
             cv2.waitKey(1)
-            vidgen.write(hr_frame[0])
 
-    vidcap.release()
-    vidbic.release()
-    vidgen.release()
-    cv2.destroyAllWindows()
+            if frame_count == 0:
+                # Define the codec and create VideoWriter objects
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                write_shape = (sr_joined.shape[1], sr_joined.shape[0])
+                print("Output resolution = {}".format(write_shape))
+                video_writer = cv2.VideoWriter('samples/evaluate/joined.avi', fourcc, 20.0, write_shape)
 
-def join_videos():
-    vidbic = cv2.VideoCapture('samples/evaluate/bic.avi')
-    vidgen = cv2.VideoCapture('samples/evaluate/gen.avi')
-    if not vidbic.isOpened() or not vidgen.isOpened():
-        raise IOError(("Couldn't open video file or webcam. If you're "
-                       "trying to open a webcam, make sure your video_path is an integer!"))
+            print("Writing frame {}".format(frame_count))
+            video_writer.write(sr_joined)
 
-    width = int(vidbic.get(cv2.CAP_PROP_FRAME_WIDTH) + vidbic.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(vidbic.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_writer.release()
 
-    # Define the codec and create VideoWriter objects
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    vidjoined = cv2.VideoWriter('samples/evaluate/joined.avi', fourcc, 20.0, (width, height))
+def process_videos(video_paths):
+    lr_method = Bicubic(0.25)
+    sr_methods = [Bicubic(4), SRGAN()]
 
-    while True:
-        flag1, frame1 = vidbic.read()
-        flag2, frame2 = vidgen.read()
-        if not flag1 or not flag2:
-            print("Done!")
-            return
-
-        joined_frame = np.concatenate((frame1, frame2), axis=1)
-        print(joined_frame.shape)
-        vidjoined.write(joined_frame)
-
-    vidbic.release()
-    vidgen.release()
-    vidjoined.release()
+    for path in video_paths:
+        process_video(path, lr_method, sr_methods, frame_skip=2)
 
 if __name__ == '__main__':
     import argparse
@@ -133,7 +107,8 @@ if __name__ == '__main__':
 
     #shrink_video('data2017/Netflix_Aerial_4096x2160_60fps_10bit_420.y4m')
     #video_superresolution(args.video)
-    join_videos()
+    #join_videos()
+    process_videos([args.video])
 
     delta_time = timer() - start_time
     print("took: %4.4fs" % delta_time)
